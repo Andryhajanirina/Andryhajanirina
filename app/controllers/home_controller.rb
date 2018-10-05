a0e44b1d850fcf38f6a38267ab19bc9f4e9707b6
Andryhajanirina <andryhaj@gmail.com>
2018-10-05 10:56:53 +0300

    Meta description et image

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,6 @@ class HomeController < ApplicationController
   	@user = User.last
   	@firstnam = @user.firstname.split(" ")
   	@firstname = @firstnam[0]
-  	@age = (Date.today.year) - (@user.birthday.year)
+  	@age = (Date.today.year) - (@user.birthday.year)  	
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,14 @@ module ApplicationHelper
 	def user_info
 		return User.last
 	end
+
+	# Personnalisé la description dans les metas pour chaqhe page
+	def page_description(description)
+		base_description = "Je m'appelle #{user_info.firstname} #{user_info.lastname}, 31 ans, née & grandit à Antananarivo (Madagascar). Je suis passionné par le développement web, plutôt à l'aise avec le back-end."
+		if description.empty?
+			return base_description
+		else
+			return description
+		end
+	end
 end
